fbcde39cf24922057ef11052b99e5b131744ae59
Angelito Casagrande <angelito.casagrande@maisinteligencia.com.br>
2019-08-13 11:34:27 -0300

    Adds application files

---
+++ src/pages/Language/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+export const Title = styled.h1``;
+export const Line = styled.div`
+  padding-bottom: 2px;
+  font-weight: bold;
+`;
+export const Header = styled.div`
+  background: #333;
+`;
