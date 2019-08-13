8f730efb9c9d8e58d1318dbfed1e41eabc0410a1
Angelito Casagrande <angelito@angelito.com.br>
2019-08-13 14:24:06 -0300

    First build

---
+++ docs/precache-manifest.ce2bfc13e3c74977a62cb998382d4773.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248f86df1dcacc6c04d86fbf2b0d5cda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3f3ed0d7a01f26bbd71",
+    "url": "2.0a5f6381.chunk.js"
+  },
+  {
+    "revision": "8f5bf2d616b88e4074d4",
+    "url": "main.f3b06bf4.chunk.js"
+  },
+  {
+    "revision": "ab818137c5d1f7a80d22",
+    "url": "runtime~main.980ef8ff.js"
+  }
+]);
