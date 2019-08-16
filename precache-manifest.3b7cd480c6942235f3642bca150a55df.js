cfee33e1777397565bb8c51c628b31d1c9b1868c
Angelito Casagrande <angelito@angelito.com.br>
2019-08-16 09:03:02 -0300

    build

---
+++ precache-manifest.3b7cd480c6942235f3642bca150a55df.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93d91782fb2369134ebb6f8a492dab2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1b015d6d7323c61ad1f",
+    "url": "./static/js/2.22ac56c3.chunk.js"
+  },
+  {
+    "revision": "0f9889ad667aa7cb6bad",
+    "url": "./static/js/main.c94441b8.chunk.js"
+  },
+  {
+    "revision": "5f6bc73d6e53448928a8",
+    "url": "./static/js/runtime~main.3d805911.js"
+  }
+]);
